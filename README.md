cfb9ed6ee9271fba231d41cdc7e94811f8e4834a
Unknown <vatsapatel13@gmail.com>
2020-04-24 22:59:16 -0700

    donezo

--- README.md
+++ README.md
@@ -1,1 +1,132 @@
-cmpe273-assignment2
+
+# Assignment 2
+Please import the [Postman Collection](/Assignment%202.postman_collection.json) abve to request the server.
+
+# API Docs
+
+## Create a test
+
+_Request_
+
+> POST http://localhost:5000/api/tests
+
+```json
+{
+    "subject": "Math",
+    "answer_keys": {
+        "1": "A",
+        "2": "B",
+        "3": "C",
+        "..": "..",
+        "49": "D",
+        "50": "E"
+    }
+}
+```
+
+_Response_
+
+```json
+201 Created
+
+{
+    "test_id": 1,
+    "subject": "Math",
+    "answer_keys": {
+        "1": "A",
+        "2": "B",
+        "3": "C",
+        "..": "..",
+        "49": "D",
+        "50": "E"
+    },
+    "submissions": [] 
+}
+```
+
+
+## Upload a scantron
+
+_Request_
+
+> POST http://localhost:5000/api/tests/1/scantrons
+
+
+```
+curl -F 'data=@path/to/local/scantron-1.json' http://localhost:5000/api/tests/1/scantrons
+```
+
+_Response_
+
+```json
+201 Created
+
+{
+    "scantron_id": 1,
+    "scantron_url": "http://localhost:5000/files/scantron-1.json",
+    "name": "Foo Bar",
+    "subject": "Math",
+    "score": 40,
+    "result": {
+        "1": {
+            "actual": "A",
+            "expected": "B"
+        },
+        "..": {
+            "actual": "..",
+            "expected": ".."
+        },
+        "50": {
+            "actual": "E",
+            "expected": "E"
+        }
+    }
+}
+```
+
+## Check all scantron submissions
+
+_Request_
+
+> GET http://localhost:5000/api/tests/1
+
+
+_Response_
+
+```json
+{
+    "test_id": 1,
+    "subject": "Math",
+    "answer_keys": {
+        "1": "A",
+        "2": "B",
+        "3": "C",
+        "..": "..",
+        "49": "D",
+        "50": "E"
+    },
+    "submissions": [
+        {
+            "scantron_id": 1,
+            "scantron_url": "http://localhost:5000/files/1.pdf",
+            "name": "Foo Bar",
+            "subject": "Math",
+            "score": 40,
+            "result": {
+                "1": {
+                    "actual": "A",
+                    "expected": "B"
+                },
+                "..": {
+                    "actual": "..",
+                    "expected": ".."
+                },
+                "50": {
+                    "actual": "E",
+                    "expected": "E"
+                }
+            }
+        }
+    ] 
+}
+```
--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask, escape, request, jsonify, g
+import os, json
+from flask import Flask, escape, request, jsonify, g, flash, redirect, url_for, send_file
+from werkzeug.utils import secure_filename
 import sqlite3
 
-id = 1001
-c_id = 112200
-students = {1001: 'Vatsa Patel', 1003: 'Vsrgatsa Patel',1006: 'Vasfgatsa Patel',1007: 'Vatsa Patel',}
-
-classes = {1001: {'name': 'cmpe', 'students': []}}
-
-
-# conn = sqlite3.connect('my_database.sqlite')
-# cursor = conn.cursor()
-# print("Opened database successfully")
+UPLOAD_FOLDER = 'uploads/scantrons/'
+ALLOWED_EXTENSIONS = {'json'}
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -27,11 +27,17 @@ def get_db():
     return db
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 @app.route('/')
 def api_hello():
@@ -40,6 +46,11 @@ def api_hello():
     else:
         return 'API works!'
 
+@app.route('/uploads/scantrons/<filename>',methods=['GET'])
+def downloadFile(filename):
+    path = os.path.join(UPLOAD_FOLDER, filename)
+    return send_file(path, as_attachment=True)
+
 @app.route('/api/tests', methods=['POST'])
 def insert_tests():
     subject = request.json['subject']
@@ -47,7 +58,7 @@ def insert_tests():
 
     # print(answer_keys)
     a = 'INSERT INTO `subjects`("name") VALUES ("' + subject + '");'
-    print(a)
+    # print(a)
     conn = sqlite3.connect('my_database.sqlite')
     cur = conn.cursor()
     cur.execute(a)
@@ -57,62 +68,124 @@ def insert_tests():
     for row in cur.execute('select seq from sqlite_sequence where name="subjects"'):
         sub_id = row[0]
 
-        print(dict(answer_keys))
-    # for k,v in answer_keys:
-    #     z = 'INSERT INTO `answer_key`(subject_id","qno","asnwer") VALUES ({},{},{})'.format(sub_id,k,'v')
-    #     print(z)
-    # cur.execute(z)
+    z = ''
+    for k, v in answer_keys.items():
+        z += 'INSERT INTO `answer_key`("subject_id","qno","answer") VALUES ({},{},"{}");'.format(sub_id, k, v)
+
+    cur.executescript(z)
+    conn.close()
+
+    return jsonify(
+        test_id=sub_id,
+        subject=subject,
+        answer_keys=answer_keys,
+        submissions=[]
+    ), 201
+
+
+@app.route('/api/tests/<sub_id>/scantrons', methods=['POST'])
+def get_id(sub_id):
+    if 'data' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['data']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+
+        data = json.load(file)
+        subject = data['subject']
+        name = data['name']
+        answer_keys = data['answers']
+
+        a = 'INSERT INTO "submissions"("sub_id","subject","name","scantron_url") VALUES ({},"{}","{}",NULL);'.format(
+            sub_id, subject, name)
+        # print(a)
+        conn = sqlite3.connect('my_database.sqlite')
+        cur = conn.cursor()
+        cur.execute(a)
+        conn.commit()
+
+        cur.execute('select seq from sqlite_sequence where name="submissions"')
+        subm_id = cur.fetchone()[0]
+        filename = str(subm_id) + '_' + secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        url= "http://127.0.0.1:5000/{}".format(UPLOAD_FOLDER)+filename
+
+        z = ''
+        for k, v in answer_keys.items():
+            z += 'INSERT INTO `submitted_answers`("subject_id","qno","answer") VALUES ({},{},"{}");'.format(subm_id, k,
+                                                                                                            v)
+
+        # print(z)
+        cur.executescript(z)
+        conn.commit()
+
+        score = 0
+        check = {}
+        for i in range(1, 51):
+            x = 'SELECT answer_key.qno, answer_key.`answer`,submitted_answers.answer FROM `answer_key`,`submitted_answers` WHERE (answer_key.subject_id={} AND submitted_answers.subject_id={} AND answer_key.qno={} AND submitted_answers.qno={});'.format(
+                sub_id, subm_id, i, i)
+            cur.execute(x)
+            row = cur.fetchone()
+            if isinstance(row, tuple):
+                check.update({i: {"actual": row[1], "expected": row[2]}})
+                if (row[1] == row[2]):
+                    score += 1
+
+        cur.execute('UPDATE submissions set score={},scantron_url="{}" WHERE id={}'.format(score, url, subm_id))
+        conn.commit()
+        conn.close()
+
+        return jsonify(
+            scantron_id=subm_id,
+            scantron_url=url,
+            name=name,
+            subject=subject,
+            score=score,
+            result=check
+        ), 201
+
+
+@app.route('/api/tests/<sub_id>', methods=['GET'])
+def get_sub(sub_id):
+    conn = sqlite3.connect('my_database.sqlite')
+    cur = conn.cursor()
+    cur.execute('SELECT qno,answer from answer_key where subject_id={}'.format(sub_id))
+    answer_key = cur.fetchall()
+
+    cur.execute('SELECT name from subjects where id={}'.format(sub_id))
+    name = cur.fetchone()[0]
+
+    submissions = []
+    cur.execute('SELECT id,name,subject,score,scantron_url from submissions where sub_id={}'.format(sub_id))
+    rows = cur.fetchall()
+    for row in rows:
+        check = {
+            'scantron_id': row[0],
+            'scantron_url': row[4],
+            "name": row[1],
+            "subject": row[2],
+            "score": row[3],
+            "results": {}
+        }
+
+        for i in range(1, 51):
+            x = 'SELECT answer_key.qno, answer_key.`answer`,submitted_answers.answer FROM `answer_key`,`submitted_answers` WHERE (answer_key.subject_id={} AND submitted_answers.subject_id={} AND answer_key.qno={} AND submitted_answers.qno={});'.format(
+                sub_id, row[0], i, i)
+            cur.execute(x)
+            row1 = cur.fetchone()
+            if isinstance(row1, tuple):
+                check["results"].update({i: {"actual": row1[1], "expected": row1[2]}})
+
+        submissions.append(check)
 
     conn.close()
 
-    # for user in query_db('select * from subjects'):
-    #     print(user[0], 'has the id', user[1])
-
-
     return jsonify(
-        user=sub_id,
-        subject=subject,
-        answer_keys=answer_keys,
-    ), 201
-
-@app.route('/students/<id>', methods=['GET'])
-def get_id(id):
-    return jsonify(
-        id = id,
-        name=students.get(int(id)),
+        test_id=sub_id,
+        subject=name,
+        answer_keys=dict(answer_key),
+        submissions=submissions
     ), 200
-
-@app.route('/classes/<id>', methods=['GET'])
-def get_class(id):
-    t=classes.get(int(id))
-    return jsonify(
-        id = id,
-        name=t['name'],
-        students=t['students']
-    ), 200
-
-@app.route('/classes', methods=['POST'])
-def insert_class():
-    name = request.json['name']
-    global c_id
-    c_id += 1
-    classes[c_id] = { 'name': name, 'students': [] }
-    print(classes)
-    return jsonify(
-        id = c_id,
-        name=name,
-        students= '[]'
-    ), 201
-
-@app.route('/classes/<class_id>', methods=['PATCH'])
-def insert_studsent(class_id):
-    stu_id = request.json['student_id']
-    s_name = students.get(stu_id)
-    classes[int(class_id)]['students'].append( {'id' : stu_id, 'name': s_name} )
-    c_metadata = classes.get(int(class_id))
-    print(c_metadata)
-    return jsonify(
-        id = class_id,
-        name= c_metadata['name'],
-        students= c_metadata['students']
-    ), 201
